12a5ddb85e2becc5e2f17b71a2bf54f0aabd627b
Alex Candido <alex.candido.tec@gmail.com>
2022-06-13 22:20:37 -0300

    aumentou slidespergroup do swaper

--- src/index.js
+++ src/index.js
@@ -128,7 +128,7 @@ function handleMutation(mutation) {
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
-  slidesPerGroup: 3,
+  slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
